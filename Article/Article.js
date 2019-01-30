17cfb049c4ed1bd6216e194207df68deee38ef39
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-01-30 14:52:59 -0700

    When 'Expand' is clicked, it is replaced by 'collapse'.

--- Article/Article.js
+++ Article/Article.js
@@ -5,16 +5,20 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
+    this.panelButtons = domElement.querySelector('.panelButtons');
     this.expandButton = domElement.querySelector('.expandButton');
+    this.collapseButton = domElement.querySelector('.collapseButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.panelButtons.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    this.expandButton.classList.toggle('expandButtonOff');
+    this.collapseButton.classList.toggle('collapseButtonOn');
   }
 }
 
